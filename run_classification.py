5f5d060c1acff62f916953699631c689aadaa76c
MATTIA ROSSO s294711 <s294711@studenti.polito.it>
2023-08-30 16:33:11 +0200

    mocap classifcation with time kernel

--- run_classification.py
+++ run_classification.py
@@ -9,30 +9,28 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 import numpy as np
-from bsgp.models import ClassificationModel
 import argparse
 import tensorflow as tf
-tf.compat.v1.disable_eager_execution()
 import json
 from sklearn.model_selection import train_test_split
-
+from sklearn.model_selection import KFold
+from src.models import VariationalGaussianProcess
+from src.utils import plot_precision_matrix, mocap_sensors_info
 from torch.utils.data import  TensorDataset
 
 import torch
 from pprint import pprint
 
-def next_path(path_pattern):
-    i = 1
-    while os.path.exists(path_pattern % i):
-        i = i * 2
-    a, b = (i / 2, i)
-    while a + 1 < b:
-        c = (a + b) / 2 # interval midpoint
-        a, b = (c, b) if os.path.exists(path_pattern % c) else (a, c)
-    directory = path_pattern % b
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    return directory
+import seaborn as sns
+import pandas as  pd
+import matplotlib.pyplot as plt
+import tensorflow_probability as tfp
+
+def save_results(args, model):
+    plot_precision_matrix(path=args.results_path, 
+                          filename='precision_matrix',
+                          P=model.model.kernel.precision(), 
+                          features=args.features)
 
 
 def set_seed(seed):
@@ -41,97 +39,116 @@ def set_seed(seed):
     np.random.seed(seed)
     tf.compat.v1.set_random_seed(seed)
 
-def create_dataset(dataset, fold):
-    dataset_path = ('./data/' + dataset + '.pth')
-    logger.info('Loading dataset from %s' % dataset_path)
-    dataset = TensorDataset(*torch.load(dataset_path))
-    X, Y = dataset.tensors
-    X, Y = X.numpy(), Y.numpy()
-    #N = X.shape[0]
-    #T = X.shape[1]
-    #D = X.shape[2]
-    #X = X.reshape(N, T*D)
+def select_first_measure(x):
+    N, T, D = x.shape
+    d = 21
+    x_stacked = np.vstack([x[i] for i in range(x.shape[0])])
+    x_stacked_first_measure = np.empty((N*T, d))
+    i = 0
+    j = 0
+    _, measures_per_sensor_50 = mocap_sensors_info('small')
+    for sensor_n_measures in measures_per_sensor_50:
+        x_stacked_sensor = x_stacked[:,i:i+sensor_n_measures] # NT x S
+        x_ = x_stacked_sensor[:,0:1]
+        x_stacked_first_measure[:,j:j+1] = x_
+        j += 1
+        i += sensor_n_measures
+    x_ = np.concatenate([np.expand_dims(x_stacked_first_measure[i * T:(i + 1) * T], 0) for i in range(N)], 0)
+    return x_
 
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.20, random_state=fold)
-    Y_train_mean, Y_train_std = Y_train.mean(0), Y_train.std(0) + 1e-9
+def create_mocap_dataset(subsampling=-1, features='full', static=False, fold=0):
+    mocap09 = np.load('./data/mocap/mocap09.npz')['data']
+    mocap07 = np.load('./data/mocap/mocap07.npz')['data']
+    mocap08 = np.load('./data/mocap/mocap08.npz')['data']
+    mocap16 = np.load('./data/mocap/mocap16.npz')['data']
 
-    return X_train, Y_train, X_test, Y_test, Y_train_mean, Y_train_std
+    #  Sub-sampling of 1/8 [ss]
+    if subsampling != -1:
+        rate = subsampling
+        mask = np.zeros(mocap09.shape[1], dtype=int)
+        mask[::rate] = 1
+        mocap09 = mocap09[:,mask==1,:]
+        mask = np.zeros(mocap08.shape[1], dtype=int)
+        mask[::rate] = 1
+        mocap08 = mocap08[:,mask==1,:]
+        mask = np.zeros(mocap07.shape[1], dtype=int)
+        mask[::rate] = 1
+        mocap07 = mocap07[:,mask==1,:]
+        mask = np.zeros(mocap16.shape[1], dtype=int)
+        mask[::rate] = 1
+        mocap16 = mocap16[:,mask==1,:]
 
-def assign_pathname(filepath, dataset, precise_kernel):
-    p = filepath + dataset + '_'
-    return p + 'AID_results.json'  if precise_kernel else p + 'ARD_results.json'
+    # Level the second dimension (time)
+    T = np.min([mocap09.shape[1], mocap07.shape[1], mocap08.shape[1], mocap16.shape[1]])
+    mocap09 = mocap09[:,0:T,:]
+    mocap07 = mocap07[:,0:T,:]
+    mocap08 = mocap08[:,0:T,:]
+    mocap16 = mocap16[:,0:T,:]
+    walk = mocap09.shape[0] + mocap16.shape[0]
+    run =  mocap07.shape[0] +  mocap08.shape[0]
 
-def save_results(filepath, onefold_data, precise_kernel):
-    results = dict()
-    results['model'] = 'bsgp'
-    results['num_inducing'] = args.num_inducing
-    results['minibatch_size'] = args.minibatch_size
-    results['n_layers'] = 1
-    results['prior_type'] = 'normal'
-    results['fold'] = args.fold
-    results['dataset'] = args.dataset
-    results['test_mnll'] = onefold_data['test_mnll']
-    results['test_accuracy'] = onefold_data['test_accuracy']
-    results['precise_kernel'] = precise_kernel
+    if features == 'small':
+        mocap09 = select_first_measure(mocap09)
+        mocap07 = select_first_measure(mocap07)
+        mocap08 = select_first_measure(mocap08)
+        mocap16 = select_first_measure(mocap16)
 
-    pprint(results)
-    jsonfilepath = assign_pathname(filepath, args.dataset, precise_kernel)
-    if precise_kernel:
-        results['prior_precision_type'] = args.prior_precision_type
-        if args.prior_precision_type == 'laplace' or args.prior_precision_type == 'laplace-diagnormal':
-            results['prior_laplace_b'] = args.prior_laplace_b
-        results['posterior_samples_kern_L'] = onefold_data['trained_model'].posterior_samples_kern_L
+    labels = np.concatenate((np.ones(run), np.zeros(walk)))[:,None]
+    Y = np.vstack((mocap09, mocap16, mocap07, mocap08))
+
+    # Normalize
+    data_std = Y[:, :].std((0, 1), keepdims=True) + 1e-5
+    data_mean = Y[:, :].mean((0, 1), keepdims=True)
+    Y = (Y - data_mean) / data_std
+
+    # Treat zero readings
+    if features == 'small':
+        Y[:,:,(7,11)] = 1e-6
     else:
-        results['posterior_samples_loglengthscales'] = onefold_data['trained_model'].posterior_samples_kern_L
-    results['posterior_samples_kern_logvar'] = onefold_data['trained_model'].posterior_samples_kern_logvar
-    with open(jsonfilepath, 'w', encoding='utf-8') as f:
-        json.dump(results, f, ensure_ascii=False, indent=4)
+        Y[:,:,(24, 25, 31, 32)] = 1e-6
+
+    # Reshape
+    D = Y.shape[2]
+    T = Y.shape[1]
+    Y = Y.reshape(Y.shape[0],-1)
+
+    # Split
+    if static == False:
+        return Y, labels, {'D': D, 'T': T}
+    else:
+        Y_train_indices_boolean = np.random.choice([1, 0], size=Y.shape[0], p=[0.8, 0.2])
+        X_train_indices = np.where(Y_train_indices_boolean == 1)[0]
+        X_test_indices = np.where(Y_train_indices_boolean == 0)[0]
+        Y_train = Y[X_train_indices]
+        Y_test = Y[X_test_indices]
+        labels_train = labels[X_train_indices]
+        labels_test = labels[X_test_indices]
+        return Y_train, labels_train, Y_test, labels_test, {'D': D, 'T': T}
 
 def main():
     set_seed(0)
+    # Load data
+    Y, labels, Yshape = create_mocap_dataset(subsampling=args.sub_sampling, features=args.features, static=False, fold=0)
+    # Train model
+    model = VariationalGaussianProcess({'Y':Y ,'labels': labels}, Yshape['D'], Yshape['T'], args.kernel_computation_type)
+    model.train(args.max_iter)
+    # Save results
+    train_accuracy = model.compute_accuracy(Y, labels)
+    print('Train Accuracy: %.2f%%'%(train_accuracy*100))
+    save_results(args, model)
+    
 
-    filepath = next_path(os.path.dirname(os.path.realpath(__file__)) + '/results/' + '/run-%04d/')
-    X_train, Y_train,  X_test, Y_test, Y_train_mean, Y_train_std = create_dataset(args.dataset, args.fold)
-    if args.minibatch_size > len(X_train): args.minibatch_size = len(X_train)
-    test_mnll, test_accuracy, model = train_model(X_train, Y_train,  X_test, Y_test,  Y_train_mean, Y_train_std, precise_kernel=args.precise_kernel) 
-    onefold_data = {'test_mnll': test_mnll, 'test_accuracy': test_accuracy, 'trained_model': model}
-    save_results(filepath, onefold_data, args.precise_kernel)
-
-def train_model(X_train, Y_train,  X_test, Y_test, Y_train_mean, Y_train_std, precise_kernel=False):
-    model = ClassificationModel('normal')
-    model.ARGS.num_inducing = args.num_inducing
-    model.ARGS.minibatch_size = args.minibatch_size
-    model.ARGS.iterations = args.iterations
-    model.ARGS.n_layers = 1
-    model.ARGS.num_posterior_samples = args.num_posterior_samples
-    model.ARGS.prior_type = 'normal'
-    model.ARGS.full_cov = False
-    model.ARGS.posterior_sample_spacing = 32
-    model.ARGS.precise_kernel = precise_kernel 
-    model.ARGS.prior_precision_type = args.prior_precision_type
-    model.ARGS.prior_laplace_b = args.prior_laplace_b
-    logger.info('Number of inducing points: %d' % model.ARGS.num_inducing)
-    model.fit(X_train, Y_train, epsilon=args.step_size)
-
-    test_mll = model.calculate_density(X_test, Y_test, Y_train_mean, Y_train_std).mean().tolist()
-    test_accuracy = model.calculate_accuracy(X_test, Y_test, Y_train_mean, Y_train_std)
-    return test_mll, test_accuracy, model 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run classification experiment')
-    parser.add_argument('--num_inducing', type=int, default=100)
-    parser.add_argument('--minibatch_size', type=int, default=1000)
-    parser.add_argument('--iterations', type=int, default=10000)
-    parser.add_argument('--dataset', type=str, required=True)
-    parser.add_argument('--fold', type=int, default=0)
-    parser.add_argument('--num_posterior_samples', type=int, default=512)
-    parser.add_argument('--step_size', type=float, default=0.01)
-    parser.add_argument('--precise_kernel', type=int, default=0)
-    parser.add_argument('--kfold', type=int, default=-1) 
-    parser.add_argument('--prior_precision_type', choices=['laplace','laplace+diagnormal', 'horseshoe+diagnormal', 'invwishart'], default=None) # Prior on kernel precision matrix
-    parser.add_argument('--prior_laplace_b', type=float, default=0.01)
-    
-    
+    parser.add_argument('--features', choices=['full','small'], default='full')
+    parser.add_argument('--sub_sampling', type=int, default=-1)
+    parser.add_argument('--results_path', type=str, default='./results')
+    parser.add_argument('--kernel_computation_type', choices=['single_sum', 'double_sum'], default='single_sum')
+    parser.add_argument('--max_iter', type=int, default=10000)
+
+
+
     args = parser.parse_args()
 
     main()
